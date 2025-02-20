fb48d4a12592029672526ad1a3c8c9045e0fbf0d
jisukimtech <jisukim.tech@gmail.com>
2025-02-20 11:34:04 +0900

    fix: OrderItem 엔티티 수정
    - 누락된 PRICE, QUANTITY, IMAGE_PATH 칼럼 추가

--- src/main/java/com/shop/coffee/orderitem/entity/OrderItem.java
+++ src/main/java/com/shop/coffee/orderitem/entity/OrderItem.java
@@ -3,6 +3,7 @@ package com.shop.coffee.orderitem.entity;
 import com.shop.coffee.global.entity.BaseEntity;
 import com.shop.coffee.item.entity.Item;
 import com.shop.coffee.order.entity.Order;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
@@ -23,5 +24,14 @@ public class OrderItem extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(length = 255, nullable = true)
+    private String imagePath;
 
 }
