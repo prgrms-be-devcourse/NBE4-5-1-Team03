29fbf004cdda573b853dd6ad2af394a9bfa32e2b
HyunmoPark <gusah0p@gmail.com>
2025-02-21 12:18:24 +0900

    fix: 누락 파일 커밋
    - 삭제된 DataInitializer.java 추가
    
    - apllication.properties 기존상태로 되돌림
    
    - itemServiceTest3 으로 테스트코드 이름변경

--- src/test/java/com/shop/coffee/item/service/itemServiceTest3.java
+++ src/test/java/com/shop/coffee/item/service/itemServiceTest3.java
@@ -18,7 +18,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-class ItemServiceTest {
+class itemServiceTest3 {
 
     @Mock
     private ItemRepository itemRepository;
