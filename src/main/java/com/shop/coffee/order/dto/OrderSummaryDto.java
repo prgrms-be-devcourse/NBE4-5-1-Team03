2ae5c907cc093dc09f800bfc39c616885d4bb422
joonhoMin <103029787+joonhoMin@users.noreply.github.com>
2025-02-24 08:25:02 +0900

    feat: 관리자주문조회 상태버튼 및  최순신순보기 추가 (#44)

--- src/main/java/com/shop/coffee/order/dto/OrderSummaryDto.java
+++ src/main/java/com/shop/coffee/order/dto/OrderSummaryDto.java
@@ -4,24 +4,27 @@ import com.shop.coffee.order.OrderStatus;
 import com.shop.coffee.order.entity.Order;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Getter
 public class OrderSummaryDto {
     private final Long id;
     private final OrderStatus orderStatus;
     private final String email;
-    private final String modifiedAt;
     private final int totalPrice;
     private final String summaryItemName;
     private final String summaryImagePath;
+    private final String shippingStartDate;
 
     public OrderSummaryDto(Order order) {
         this.id = order.getId();
         this.orderStatus = order.getOrderStatus();
         this.email = order.getEmail();
-        this.modifiedAt = String.valueOf(order.getModifiedAt());
         this.totalPrice = order.getTotalPrice();
         this.summaryItemName = getSummaryItemName(order);
         this.summaryImagePath = getSummaryImagePath(order);
+        this.shippingStartDate = calculateShippingStartDate(order.getModifiedAt());
     }
 
     private String getSummaryItemName(Order order) {
@@ -37,5 +40,19 @@ public class OrderSummaryDto {
             return order.getOrderItems().getFirst().getImagePath();
         }
         return null;
+    }
+
+    private String calculateShippingStartDate(LocalDateTime modifiedAt) {
+        if(modifiedAt == null) {
+            return null;
+        }
+
+        LocalDateTime cutoffTime = modifiedAt.toLocalDate().atTime(14, 0);
+
+        LocalDateTime shippingDate = modifiedAt.isAfter(cutoffTime)
+                ? modifiedAt.plusDays(1)
+                : modifiedAt;
+
+        return shippingDate.format(DateTimeFormatter.ofPattern("MM. dd(E)"));
     }
 }
