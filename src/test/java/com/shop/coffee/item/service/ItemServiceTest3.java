763bcb74dc9630626fe6300f94ea7c1d08d59d2b
HyunmoPark <gusah0p@gmail.com>
2025-02-23 13:25:33 +0900

    test: 상품 단건 조회 테스트 코드 추가

--- src/test/java/com/shop/coffee/item/service/ItemServiceTest3.java
+++ src/test/java/com/shop/coffee/item/service/ItemServiceTest3.java
@@ -18,7 +18,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-class itemServiceTest3 {
+class ItemServiceTest3 {
 
     @Mock
     private ItemRepository itemRepository;
