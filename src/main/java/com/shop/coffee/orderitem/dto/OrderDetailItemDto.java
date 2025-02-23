62f13f01057b9ff6f51c171151fed32a61949b1b
SeongCheol Jung <kir5121@naver.com>
2025-02-23 17:10:19 +0900

    feat: 주문 상세 페이지 조회를 위한 OrderDetail관련 DTO 추가 및 변경

---
+++ src/main/java/com/shop/coffee/orderitem/dto/OrderDetailItemDto.java
@@ -0,0 +1,30 @@
+package com.shop.coffee.orderitem.dto;
+
+import com.shop.coffee.item.dto.ItemDto;
+import com.shop.coffee.orderitem.entity.OrderItem;
+import lombok.Getter;
+
+@Getter
+public class OrderDetailItemDto {
+
+    private Long id;
+
+    private Long itemId;
+
+    private String itemName;
+
+    private int price;
+
+    private int quantity;
+
+    private String imagePath;
+
+    public OrderDetailItemDto(OrderItem orderItem) {
+        this.id = orderItem.getId();
+        this.itemId = orderItem.getItem().getId();
+        this.itemName = orderItem.getItem().getName();
+        this.price = orderItem.getPrice();
+        this.quantity = orderItem.getQuantity();
+        this.imagePath = orderItem.getImagePath();
+    }
+}
