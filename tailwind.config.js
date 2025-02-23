3c5d613fb6e573b2705468425076026eecf37f1d
jisukimtech <jisukim.tech@gmail.com>
2025-02-24 00:45:34 +0900

    style(tailwind): 커스텀 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./src/main/resources/templates/**/*.html"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-      require('daisyui'),
-  ],
-}
+    content: [
+        "./src/main/resources/templates/**/*.html",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                customWhite: '#DDDDDD',
+                customBlack: '#2C2C2C',
+                customBlue: '#4880EE',
+                customSkyBlue: '#30B0C7',
+                customGray: '#D9D9D9'
+            },
+        },
+    },
+    plugins: [
+        require('daisyui'),
+    ],
+};
