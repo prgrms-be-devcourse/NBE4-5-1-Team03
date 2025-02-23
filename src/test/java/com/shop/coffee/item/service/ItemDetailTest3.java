ee23674a1a2ca9c4e3c44653d020436dc5914cca
HyunmoPark <gusah0p@gmail.com>
2025-02-23 13:25:33 +0900

    test: 상품 단건 조회 테스트 코드 추가

---
+++ src/test/java/com/shop/coffee/item/service/ItemDetailTest3.java
@@ -0,0 +1,53 @@
+package com.shop.coffee.item.service;
+
+import com.shop.coffee.item.dto.ItemDto;
+import com.shop.coffee.item.entity.Item;
+import com.shop.coffee.item.repository.ItemRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+@Import(ItemService.class)
+class ItemDetailTest3 {
+
+    @Autowired
+    private ItemService itemService;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 단건 조회 - 성공")
+    void getItemById_success() {
+        // given
+        Item savedItem = itemRepository.save(new Item("커피A", "Category1", 100, "Description1", "/images/coffeeA.png"));
+
+        // when
+        ItemDto itemDto = itemService.getItemById(savedItem.getId());
+
+        // then
+        assertThat(itemDto).isNotNull();
+        assertThat(itemDto.getName()).isEqualTo("커피A");
+        assertThat(itemDto.getCategory()).isEqualTo("Category1");
+    }
+
+    @Test
+    @DisplayName("상품 단건 조회 - 존재하지 않는 상품(실패)")
+    void getItemById_notFound() {
+        // given
+        Long invalidItemId = 999L;
+
+        // when & then
+        assertThatThrownBy(() -> itemService.getItemById(invalidItemId))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 주문이 존재하지 않습니다.");
+    }
+}
