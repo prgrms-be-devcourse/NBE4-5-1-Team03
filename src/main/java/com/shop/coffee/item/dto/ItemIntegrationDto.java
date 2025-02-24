cdbc4aee1b9ca7733d625b3323345b4ef2ffad7d
jisukimtech <jisukim.tech@gmail.com>
2025-02-24 11:45:01 +0900

    feat: DTO에 기본 생성자 추가

--- src/main/java/com/shop/coffee/item/dto/ItemIntegrationDto.java
+++ src/main/java/com/shop/coffee/item/dto/ItemIntegrationDto.java
@@ -2,8 +2,10 @@ package com.shop.coffee.item.dto;
 
 import com.shop.coffee.item.entity.Item;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @Getter
 @Setter
 public class ItemIntegrationDto {
